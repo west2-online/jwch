b17df64e159f09fd1cda3f207832a4dbe262d2ca
FantasyRL <1845983502@qq.com>
2025-01-09 16:22:48 +0800

    feat: CI

--- plan.go
+++ plan.go
@@ -1,3 +1,19 @@
+/*
+Copyright 2024 The west2-online Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package jwch
 
 import (
@@ -26,7 +42,7 @@ func (s *Student) GetCultivatePlan() (string, error) {
 			// "ctl00$xymcdpl":                       "010",// 学院名称
 			"ctl00$dldpl":                         "<-全部->", // 大类
 			"ctl00$zymcdpl":                       "<-全部->", // 专业代码(无法获取)
-			"ctl00$zylbdpl":                       "本专业",   // 本专业、辅修
+			"ctl00$zylbdpl":                       "本专业",    // 本专业、辅修
 			"ctl00$ContentPlaceHolder1$DDL_syxw":  "<-全部->",
 			"ctl00$ContentPlaceHolder1$BT_submit": "确定",
 		})
