d88200a57187dc804f16231ccbd7e4e812d2bc56
jiuxia211 <2064166368@qq.com>
2025-02-24 17:24:41 +0800

    fix: nil pointer in get marks (#31)
    * fix: nil pointer in get marks
    
    * fix: use errno
    
    * fix: ci
    
    * fix: lint

--- mark.go
+++ mark.go
@@ -36,7 +36,15 @@ func (s *Student) GetMarks() (resp []*Mark, err error) {
 		return nil, err
 	}
 
-	list := htmlquery.Find(htmlquery.FindOne(res, `//*[@id="ContentPlaceHolder1_DataList_xxk"]/tbody`), "tr")
+	table := htmlquery.FindOne(res, `//*[@id="ContentPlaceHolder1_DataList_xxk"]/tbody`)
+	if table == nil {
+		return nil, errno.HTMLParseError.WithMessage("marks table not found")
+	}
+
+	list := htmlquery.Find(table, "tr")
+	if len(list) < 2 {
+		return nil, errno.HTMLParseError.WithMessage("insufficient table rows")
+	}
 
 	// 去除第一个元素，第一个元素是标题栏，有个判断文本是“课程名称”
 	// TODO: 我们如何确保第一个元素一定是标题栏?
